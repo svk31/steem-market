046683ac7550862b643bb3a1930b0a2545e6aa4b
Sigve Kvalsvik <sigvekvalsvik@gmail.com>
2016-07-13 09:31:01 +0200

    Add forever run script

--- server/server.js
+++ server/server.js
@@ -31,17 +31,16 @@ function renderFullPage(html, data) {
       <head>
         <title>Steemex</title>
         <meta name="description" content="Follow and trade on the Steem internal market for STEEM vs Steem Dollars" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"  />
-        <link rel="stylesheet" href="app.css"  />
         <script>
           window.__INITIAL_STATE__ = ${JSON.stringify(data)};
         </script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"  />
+        <link rel="stylesheet" href="app.css"  />
       </head>
       <body>
         <div id="content">${html}</div>
         <script src="app.js" charset="utf-8"></script>
         <script src="highcharts.js" charset="utf-8"></script>
-
       </body>
     </html>
     `
